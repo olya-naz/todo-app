70000009bfd386cd25866754fac82d3553500e7f
Olya <olya@MacBook-Pro-Olya.local>
2019-12-10 12:51:28 +0200

    Updates

---
+++ precache-manifest.e98c0a4ddde2b0d59fef4c4de684472c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a576ee5f8f29892391d8990d3764526",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "484559f7445c10d4a245",
+    "url": "/todo-app/static/css/2.95f4a6a3.chunk.css"
+  },
+  {
+    "revision": "fbaba5f38a1af9687908",
+    "url": "/todo-app/static/css/main.57843d61.chunk.css"
+  },
+  {
+    "revision": "484559f7445c10d4a245",
+    "url": "/todo-app/static/js/2.79b5ea1e.chunk.js"
+  },
+  {
+    "revision": "fbaba5f38a1af9687908",
+    "url": "/todo-app/static/js/main.5f492c6f.chunk.js"
+  },
+  {
+    "revision": "cc4560adcbcd809f8a8d",
+    "url": "/todo-app/static/js/runtime-main.e51474da.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/todo-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/todo-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/todo-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/todo-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/todo-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
